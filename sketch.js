468e5d7eefc258bb77661a7f1c05396bea25bb5f
marianabll <100840719+marianabll@users.noreply.github.com>
2022-06-28 20:59:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var sprite1
+
+
+function setup() { // CRIADORA
+  createCanvas(1000,600);
+
+  sprite1 = createSprite(100,150,30,30)
+}
+
+
+
+function draw() 
+{
+  background(180);
+  drawSprites()
+}
+
+
+
+
